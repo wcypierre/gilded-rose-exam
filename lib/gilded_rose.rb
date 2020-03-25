5e6c8a43f850a737202675e7e01c34d36e52bc95
Wong Chieh Yie <wcypierre@gmail.com>
2020-03-25 15:38:14 +0800

    Extract item sell_in into private function

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -25,7 +25,7 @@ class GildedRose
     if item.quality < 50
       item.quality = item.quality + 1
 
-      item.sell_in = item.sell_in - 1
+      item = decrease_sell_in(item)
 
       if item.sell_in < 0
         if item.quality < 50
@@ -50,7 +50,7 @@ class GildedRose
       end
     end
 
-    item.sell_in = item.sell_in - 1
+    item = decrease_sell_in(item)
 
     if item.sell_in < 0
       item.quality = item.quality - item.quality
@@ -64,7 +64,7 @@ class GildedRose
       item.quality = item.quality - 2
     end
 
-    item.sell_in = item.sell_in - 1
+    item = decrease_sell_in(item)
 
     if item.sell_in < 0
       if item.quality >= 2
@@ -80,13 +80,19 @@ class GildedRose
       item.quality = item.quality - 1
     end
 
-    item.sell_in = item.sell_in - 1
+    item = decrease_sell_in(item)
 
     if item.sell_in < 0
       if item.quality > 0
         item.quality = item.quality - 1
       end
     end
+
+    return item
+  end
+
+  private def decrease_sell_in(item)
+    item.sell_in = item.sell_in - 1
 
     return item
   end
